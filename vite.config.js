8a0f57d54dab67b5d63dbb417af29cce887e6864
Arjun G <arjungopinath06@gmail.com>
2026-05-11 23:16:07 +0530

    feat. update vite.config to remove cross origin errors

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,22 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+  plugins: [
+    {
+      name: 'classic-script',
+      transformIndexHtml(html) {
+        // itch.io's sandboxed iframe doesn't support ES module scripts;
+        // strip type="module" and crossorigin so the IIFE loads as a classic script
+        return html.replace(/<script type="module" crossorigin/g, '<script');
+      },
+    },
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'iife',
+        name: 'CaptainsWheel',
+      },
+    },
+  },
 });
